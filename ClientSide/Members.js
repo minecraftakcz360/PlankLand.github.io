f9b3a6ec7095aa56f51259d7fb3ae02b3a63e081
minecraftakcz360 <103266461+minecraftakcz360@users.noreply.github.com>
2024-08-05 16:08:40 +0200

    Update Members.js

--- ClientSide/Members.js
+++ ClientSide/Members.js
@@ -1,12 +1,137 @@
 const Members = 
 {
-    Rustycz: {
+    Rustycz: 
+    {
         Name: document.getElementById("MRustyczName"),
         DisButton: document.getElementById("MRustyczDis"),
         MinButton: document.getElementById("MRustyczMin"),
         DisName: "@rustycz",
         MinName: "Rustycz",
-    }
+    },
+    gangalf: 
+    {
+        Name: document.getElementById("MgangalfName"),
+        DisButton: document.getElementById("MgangalfDis"),
+        MinButton: document.getElementById("MgangalfMin"),
+        DisName: "@pilokim",
+        MinName: "gangalf",
+    },
+    minecraftakcz360: 
+    {
+        Name: document.getElementById("Mminecraftakcz360Name"),
+        DisButton: document.getElementById("Mminecraftakcz360Dis"),
+        MinButton: document.getElementById("Mminecraftakcz360Min"),
+        DisName: "@minecraftakcz360",
+        MinName: "minecraftakcz360",
+    },
+
+    HODMONDAK: 
+    {
+        Name: document.getElementById("MHODMONDAKName"),
+        DisButton: document.getElementById("MHODMONDAKDis"),
+        MinButton: document.getElementById("MHODMONDAKMin"),
+        DisName: "@hodmondak",
+        MinName: "HODMONDAK",
+    },
+
+    HODMONDAK: 
+    {
+        Name: document.getElementById("MliechmiName"),
+        DisButton: document.getElementById("MliechmiDis"),
+        MinButton: document.getElementById("MliechmiMin"),
+        DisName: "@liechmi",
+        MinName: "Rauded",
+    },
+
+    Rauded: 
+    {
+        Name: document.getElementById("MRaudedName"),
+        DisButton: document.getElementById("MRaudedDis"),
+        MinButton: document.getElementById("MRaudedMin"),
+        DisName: "@liechmi",
+        MinName: "Rauded",
+    },
+
+    Mandle123: 
+    {
+        Name: document.getElementById("MMandle123Name"),
+        DisButton: document.getElementById("MMandle123Dis"),
+        MinButton: document.getElementById("MMandle123Min"),
+        DisName: "@mandlos123",
+        MinName: "Mandle123",
+    },
+
+    Mandle123: 
+    {
+        Name: document.getElementById("MMandle123Name"),
+        DisButton: document.getElementById("MMandle123Dis"),
+        MinButton: document.getElementById("MMandle123Min"),
+        DisName: "@mandlos123",
+        MinName: "Mandle123",
+    },
+
+    helikopter: 
+    {
+        Name: document.getElementById("MhelikopterName"),
+        DisButton: document.getElementById("MhelikopterDis"),
+        MinButton: document.getElementById("MhelikopterMin"),
+        DisName: "@cigos_mikiez",
+        MinName: "helikopter",
+    },
+
+    smajra: 
+    {
+        Name: document.getElementById("MsmajraName"),
+        DisButton: document.getElementById("MsmajraDis"),
+        MinButton: document.getElementById("MsmajraMin"),
+        DisName: "@prasecix",
+        MinName: "smajra",
+    },
+
+    smajra: 
+    {
+        Name: document.getElementById("MNedr0SKName"),
+        DisButton: document.getElementById("MNedr0SKDis"),
+        MinButton: document.getElementById("MNedr0SKMin"),
+        DisName: "@justme1151",
+        MinName: "Nedr0SK",
+    },
+
+    MarkisCZ: 
+    {
+        Name: document.getElementById("MMarkisCZName"),
+        DisButton: document.getElementById("MMarkisCZDis"),
+        MinButton: document.getElementById("MMarkisCZMin"),
+        DisName: "@markiscz",
+        MinName: "MarkisCZ",
+    },
+
+    rudolf: 
+    {
+        Name: document.getElementById("MrudolfName"),
+        DisButton: document.getElementById("MrudolfDis"),
+        MinButton: document.getElementById("MrudolfMin"),
+        DisName: "@0llafos",
+        MinName: "rudolf",
+    },
+
+    SAURON: 
+    {
+        Name: document.getElementById("MSAURONName"),
+        DisButton: document.getElementById("MSAURONDis"),
+        MinButton: document.getElementById("MSAURONMin"),
+        DisName: "@mariomomo",
+        MinName: "SAURON",
+    },
+
+    Alexmon16: 
+    {
+        Name: document.getElementById("MAlexmon16Name"),
+        DisButton: document.getElementById("MAlexmon16Dis"),
+        MinButton: document.getElementById("MAlexmon16Min"),
+        DisName: "@alex15mon",
+        MinName: "Alexmon16",
+    },
 };
 
 
@@ -18,4 +143,37 @@ Members.Rustycz.DisButton.addEventListener("click", function()
 Members.Rustycz.MinButton.addEventListener("click", function()
 {
     Members.Rustycz.Name.textContent = Members.Rustycz.MinName;
+});
+
+
+Members.gangalf.DisButton.addEventListener("click", function()
+{
+    Members.gangalf.Name.textContent = Members.gangalf.DisName;
+});
+
+Members.gangalf.MinButton.addEventListener("click", function()
+{
+    Members.gangalf.Name.textContent = Members.gangalf.MinName;
+});
+
+
+Members.minecraftakcz360.DisButton.addEventListener("click", function()
+{
+    Members.minecraftakcz360.Name.textContent = Members.minecraftakcz360.DisName;
+});
+
+Members.minecraftakcz360.MinButton.addEventListener("click", function()
+{
+    Members.minecraftakcz360.Name.textContent = Members.minecraftakcz360.MinName;
+});
+
+
+Members.HODMONDAK.DisButton.addEventListener("click", function()
+{
+    Members.HODMONDAK.Name.textContent = Members.HODMONDAK.DisName;
+});
+
+Members.HODMONDAK.MinButton.addEventListener("click", function()
+{
+    Members.HODMONDAK.Name.textContent = Members.HODMONDAK.MinName;
 });
